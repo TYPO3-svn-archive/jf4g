acce6350bbd04378e78397f3b5aa9312090394d2
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-01 19:01:39 +0000

    Remove the mod1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jf4g/trunk@41675 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,6 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-if (TYPO3_MODE == 'BE') {
-	t3lib_extMgm::addModulePath('web_txjf4gM1', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
-	t3lib_extMgm::addModule('web', 'txjf4gM1', 'before:info', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
-}
-
 $tempColumns = array(
 	'tx_jf4g_pin' => array(
 		'exclude' => 1,
