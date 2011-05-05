96fbf27cd9dba326739092fa9d12d7831f8039ca
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-05 20:50:30 +0000

    Fix the flexform data extraction
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jf4g/trunk@47356 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_jf4g_pi1.php
+++ pi1/class.tx_jf4g_pi1.php
@@ -71,17 +71,10 @@ class tx_jf4g_pi1 extends tslib_pibase
 		$this->contentKey = "jf4g";
 
 		if ($this->cObj->data['list_type'] == $this->extKey.'_pi1') {
+
 			// It's a content, al data from flexform
-			// Set the Flexform information
-			$this->pi_initPIflexForm();
-			$piFlexForm = $this->cObj->data['pi_flexform'];
-			foreach ($piFlexForm['data'] as $sheet => $data) {
-				foreach ($data as $lang => $value) {
-					foreach ($value as $key => $val) {
-						$this->lConf[$key] = $this->pi_getFFvalue($piFlexForm, $key, $sheet);
-					}
-				}
-			}
+			
+			$this->lConf['storys'] = $this->getFlexformData('general', 'storys'); 
 
 			// define the key of the element
 			$this->contentKey .= "_c" . $this->cObj->data['uid'];
@@ -335,6 +328,42 @@ class tx_jf4g_pi1 extends tslib_pibase
 		$_EXTKEY = $key;
 		include(t3lib_extMgm::extPath($key) . 'ext_emconf.php');
 		return $EM_CONF[$key]['version'];
+	}
+
+	/**
+	 * Extract the requested information from flexform
+	 * @param string $sheet
+	 * @param string $name
+	 * @param boolean $devlog
+	 * @return string
+	 */
+	protected function getFlexformData($sheet='', $name='', $devlog=true)
+	{
+		$this->pi_initPIflexForm();
+		$piFlexForm = $this->cObj->data['pi_flexform'];
+		if (! isset($piFlexForm['data'])) {
+			if ($devlog === true) {
+				t3lib_div::devLog("Flexform Data not set", $this->extKey, 1);
+			}
+			return null;
+		}
+		if (! isset($piFlexForm['data'][$sheet])) {
+			if ($devlog === true) {
+				t3lib_div::devLog("Flexform sheet '{$sheet}' not defined", $this->extKey, 1);
+			}
+			return null;
+		}
+		if (! isset($piFlexForm['data'][$sheet]['lDEF'][$name])) {
+			if ($devlog === true) {
+				t3lib_div::devLog("Flexform Data [{$sheet}][{$name}] does not exist", $this->extKey, 1);
+			}
+			return null;
+		}
+		if (isset($piFlexForm['data'][$sheet]['lDEF'][$name]['vDEF'])) {
+			return $this->pi_getFFvalue($piFlexForm, $name, $sheet);
+		} else {
+			return $piFlexForm['data'][$sheet]['lDEF'][$name];
+		}
 	}
 }
 
